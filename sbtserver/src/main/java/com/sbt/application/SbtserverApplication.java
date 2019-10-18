fd1ee0aa6287f6d41e1a5a205c3c70e7af10ab79
mantu.gupta <mantu.gupta@SSAHMLTW03684.elitecore.co.in>
2019-10-18 23:46:49 +0530

    committing the sbt server changes

---
+++ sbtserver/src/main/java/com/sbt/application/SbtserverApplication.java
@@ -0,0 +1,13 @@
+package com.sbt.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtserverApplication.class, args);
+	}
+
+}
