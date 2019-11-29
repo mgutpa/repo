c05614047d6edb24af75f74fc47df1b6435e10fe
kumar9086 <58334150+kumar9086@users.noreply.github.com>
2019-11-29 22:45:47 +0530

    Update SbtserverApplication.java
    Changes for #1

--- sbtserver/src/main/java/com/sbt/application/SbtserverApplication.java
+++ sbtserver/src/main/java/com/sbt/application/SbtserverApplication.java
@@ -8,6 +8,7 @@ public class SbtserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbtserverApplication.class, args);
-	}
+	        System.out.println("Changes for item #1");
+     }
 
 }
